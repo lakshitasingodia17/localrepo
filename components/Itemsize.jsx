354bb3bcfbb86563e75a27ddc6663a51c6890cc2
lakshitasingodia17 <lakshita.singodia17@.in>
2025-01-29 23:49:14 +0530

    editing

--- components/Itemsize.jsx
+++ components/Itemsize.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap"
 import { useEffect, useState } from "react"
 
 export const Itemsize = ({name}) => {
-const[count,setCount]=useState(0)
+const[count,setCount]=useState(1)
 
 let hideBox;
 useEffect( ()=>{
@@ -14,7 +14,16 @@ useEffect( ()=>{
 }
 }
 )
+const [selected,setSelected]=useState("small")
 
+const productSize={
+    small:100,
+    large:200
+}
+const handleSize=(size)=>{
+    setSelected(size)
+}
+const totalPrice=productSize[selected]*count;
     return (
         <div className={`w-full ml-[-5.5vw] absolute mt-[-2vw] h-[80vh]  hidden ${name} bg-[#d6d6d63b] backdrop-blur-sm`}>
     <div className={`w-full   h-screen z-0  px-[35vw] py-[7vw] left-0 absolute flex justify-center items-center`}>
@@ -31,19 +40,20 @@ useEffect( ()=>{
             <h1 className="capitalize font-bold">quantity</h1>
             <p className="text-[1vw] text-[#5f5f5f] capitalize mt-[-1.2vw]">required <span className="text-[2vw] relative mt-[-30px] ">.</span>   select any 1 option</p></div>
             <div className="w-full h-[1px] bg-[#dbdbdb]"></div>
-            <div className="flex justify-between items-center my-3">
-                <h1 className="capitalize font-bold">small</h1>
+            {
+                Object.keys(productSize).map((size)=>(
+                    <div onClick={()=>handleSize(size)} className="flex justify-between items-center my-3">
+                <h1 className="capitalize font-bold">{size}</h1>
                 <div className="flex items-center gap-1 h-6">
-                    <h1 className="font-bold">110</h1>
-                <input className="mt-[-5px]" type="radio" name="same" /></div>
-            </div>
-            <div className="flex justify-between items-center my-2">
-                <h1 className="capitalize font-bold">large</h1>
-                <div className="flex items-center gap-1 h-6">
-                <h1 className="capitalize font-bold">120</h1>
-                <input className="mt-[-5px] focus:ring-indigo-900" type="radio" name="same" />
+                    <h1 className="font-bold">{}</h1>
+                <input className={`mt-[-5px] `} type="radio" name="same" />
                 </div>
             </div>
+                ))
+            }
+            
+            
+        
             
         </div>
         <div className="bg-white rounded-lg mt-3 w-[96%] ml-[2%]  py-2 px-2">
@@ -65,7 +75,7 @@ useEffect( ()=>{
 }} className="text-[1.6vw]">+</button>
                 </div>
                 <div className="w-[70%] h-full pt-[17px] px-2 pb-[9px]">
-                    <button className="text-[1.3vw] w-full h-full rounded-lg bg-[#ff933b]">Add item price</button>
+                    <button className="text-[1.3vw] w-full h-full rounded-lg bg-[#ff933b]">Add item {totalPrice}</button>
                 </div>
             </div>
        </div>
